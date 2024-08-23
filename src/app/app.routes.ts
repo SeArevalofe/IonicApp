0ae3999157dfb9ae98bd90a9676660d4bdb0bf5c
SeArevalofe <arevalof.se@gmail.com>
2024-08-23 18:58:05 -0400

    se sube proyecto

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+ {path: 'main', loadComponent: ()=> import('./main/main.page').then(m => m.MainPage) },
+  {
+    path: 'product-list',
+    loadComponent: () => import('./product-list/product-list.page').then( m => m.ProductListPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  }
+];
